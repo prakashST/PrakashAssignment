33205dbfe266a33e8def8580ce9077a15614fcb5
prakash <prakashst0028@gmail.com>
2023-08-31 11:01:38 +0530

    Tables using loop statement

---
+++ task8-loopstatement(31-08-23)/loop.js
@@ -0,0 +1,9 @@
+
+for(num1=1;num1<=20;num1++){
+        console.log(`
+-----------------${num1}tables-------------------`)    
+    for(num2=1;num2<=20;num2++){
+        console.log(`${num2} * ${num1} = ${num1*num2}`);
+    } 
+}
+
